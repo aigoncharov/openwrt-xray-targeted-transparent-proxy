750247ac28a2a7940b094eab73cdaa9928262a3a
aigoncharov <andrey@goncharov.page>
2025-02-10 16:12:50 +0300

    Update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Removing packages..."
 opkg remove xray-core
 opkg remove v2fly-geoip v2fly-geosite
 opkg remove python3
@@ -9,10 +10,15 @@ opkg remove iptables-mod-conntrack-extra \
   iptables-mod-tproxy \
   kmod-ipt-nat6 
 
+echo "Removing config..."
 rm -f /etc/xray/vless_subscription_url
 rm -f /etc/xray/config.json
 rm -f /etc/config/xray
 
 echo "" > /etc/firewall.xraytproxy
 
+echo "Restarting firewall..."
 /etc/init.d/firewall restart
+echo "Removing network..."
+/etc/init.d/network restart
+echo "Done!"
